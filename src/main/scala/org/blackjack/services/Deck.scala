848aca05558a8dafdf2ce4913f8272e39d5a31e5
Sandeep Purohit <sandeep.purohit@lunatech.com>
2018-08-03 15:50:31 +0200

    add shuffle for the dev

--- src/main/scala/org/blackjack/services/Deck.scala
+++ src/main/scala/org/blackjack/services/Deck.scala
@@ -1,14 +1,16 @@
 package org.blackjack.services
 
 import org.blackjack.models._
+import scala.collection.mutable.ListBuffer
 
 class Deck {
 
+  val deck = new ListBuffer[Card]
 
   def initializeDeck = for {
     suit <- List(Heart,Spade,Club,Diamond)
     rank <- List(Ace,King,Queen,Jack,Ten,Nine,Eight,Seven,Six,Five,Four,Three,Two)
-  } yield new Card(suit,rank)
+  } yield deck :+ Card(suit,rank)
 
-  def shuffle = ???
+  def shuffle = scala.util.Random.shuffle(initializeDeck)
 }
